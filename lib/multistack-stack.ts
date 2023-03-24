a2a1dab164f6801231cdf17a3be77e0a42f886e2
Marc Mendoza <777marc@gmail.com>
2023-03-24 15:07:46 -0400

    init

--- lib/multistack-stack.ts
+++ lib/multistack-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as s3 from 'aws-cdk-lib/aws-s3';
+interface MultistackProps extends cdk.StackProps {
+  encryptBucket?: boolean;
+}
 export class MultistackStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: MultistackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MultistackQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    // Add a Boolean property "encryptBucket" to the stack constructor.
+    // If true, creates an encrypted bucket. Otherwise, the bucket is unencrypted.
+    // Encrypted bucket uses KMS-managed keys (SSE-KMS).
+    if (props && props.encryptBucket) {
+      new s3.Bucket(this, "MyGroovyBucket", {
+        encryption: s3.BucketEncryption.KMS_MANAGED, 
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+      });
+    } else {
+      new s3.Bucket(this, "MyGroovyBucket", {
+        removalPolicy: cdk.RemovalPolicy.DESTROY});
+    }
   }
 }
